1dafcf89bfd281c464f1a0d328e8955ef5b7c64e
Louki99 <louki.idris@gmail.com>
2025-06-16 11:18:15 +0100

    init promo engine

---
+++ src/main/java/com/foodplus/promo/PromoEngineApplication.java
@@ -0,0 +1,14 @@
+package com.foodplus.promo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoEngineApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(PromoEngineApplication.class, args);
+    }
+}
